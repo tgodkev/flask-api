dd773fcec3b88f851612bed7417b03c6632d368d
Kevin <50965376+tgodkev@users.noreply.github.com>
2023-04-09 00:56:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,8 @@ def get_recipes():
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     
-    # Updated CSS selector
-    recipe_cards = soup.select('a.comp.card--image-top.mntl-card-list-items.mntl-document-card.mntl-card.card.card--no-image')
-
-    recipes = []
-    for card in recipe_cards:
-        link = card['href']
-        title_element = card.find('span', class_='card__titleText')
-
-        if title_element and link:
-            title = title_element.text.strip()
-            recipes.append({'title': title, 'link': link})
+    recipe_cards = soup.select('span.card__title')
+    recipes = [card.text.strip() for card in recipe_cards]
     
     return recipes
 
